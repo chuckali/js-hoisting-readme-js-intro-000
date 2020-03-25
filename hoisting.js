b19666427062f7a87c8a740f4969e825ac9f0eb6
Charles Cusson <charlescusson@gmail.com>
2020-03-25 01:48:28 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,13 +11,12 @@ var lyric = "maybe";
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
-}
+  thisIsCrazy();
 
+}
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
