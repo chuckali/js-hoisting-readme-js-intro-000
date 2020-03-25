64254d94383bdc2d1439ec4624fd6140e2a1ab4e
Charles Cusson <charlescusson@gmail.com>
2020-03-25 01:57:42 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -20,11 +20,10 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
+  var name = "Kristin";
     console.log(name);
-    var name = "Kristin";
+
   }
+    sayMy();
 }
